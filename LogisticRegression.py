f96b2cfeca4d59d8f1199ce53f09e5211c182dc5
Leonardoalv201 <a01705998@itesm.mx>
2022-09-05 12:54:29 -0500

    update LogisticRegression

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 # Libreria para graficación
-import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Obtención los datos 
 df = pd.read_excel('Raisin_Dataset.xlsx')
@@ -29,17 +29,21 @@ X_train, X_test, y_train, y_test = train_test_split(X_raisin, y_raisin, random_s
 # Ejecución del modelo
 model.fit(X_train, y_train)
 
+print("Coeficiebtes del modelo:", model.coef_)
+print("Intercepción", model.intercept_)
+
 # Prediccion  usando el modelo sobre de los x_test 
 y_fit = model.predict(X_test)
+
+# Mean Square Error de los datos predichos y los reales
+print("Mean square error del modelo:", mean_squared_error(y_test, y_fit))
 
 # Reestructuración de los datos para poder graficar
 y_df_fit = pd.DataFrame(y_fit, columns = ['Predicted value'])
 y_df_test = y_test.reset_index().drop('index', axis = 1)
 y_df_fit['Real value'] = y_df_test
+y_df_fit.reset_index(inplace=True)
 
 # Grafica sobre datos predichos y datos reales
-sns.set(rc={"figure.figsize":(30, 10)})
-sns.lineplot(data = y_df_fit)
-
-# Mean Square Error de los datos predichos y los reales
-mean_squared_error(y_test, y_fit)
+y_df_fit.plot(x="index", y=["Real value", "Predicted value"])
+plt.show()
