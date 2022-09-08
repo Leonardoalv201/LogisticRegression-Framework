3ff255d31b5585118c98cb29d290ef1be19afceb
Leonardoalv201 <a01705998@itesm.mx>
2022-09-08 13:21:31 -0500

    Add Reporte Framework

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,18 +5,23 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 # Libreria para graficación
-import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Obtención los datos 
 df = pd.read_excel('Raisin_Dataset.xlsx')
 
 # Cambio de strings a int para el modelo
-df["Class"].replace({"Kecimen": 0, "Besni": 1}, inplace=True)
+df["Class"].replace({
+    "Kecimen": 0,
+    "Besni": 1
+    }, inplace=True)
 
 # Se crea el modelo de regresión logistica 
-model = LogisticRegression()
+model = LogisticRegression(penalty = 'l1', solver = 'saga')
 model
 
 # Selección de "x" y "y" del modelo
@@ -32,18 +37,34 @@ model.fit(X_train, y_train)
 print("Coeficiebtes del modelo:", model.coef_)
 print("Intercepción", model.intercept_)
 
-# Prediccion  usando el modelo sobre de los x_test 
-y_fit = model.predict(X_test)
+#Prediccion del modelo en datos de train
+y_pred_train = model.predict(X_train)
 
 # Mean Square Error de los datos predichos y los reales
-print("Mean square error del modelo:", mean_squared_error(y_test, y_fit))
+print("Mean square error del modelo en datos train:", mean_squared_error(y_train, y_pred_train))
+# Accuracy del modelo
+print("Accuracy del modelo en datos train:", accuracy_score(y_train, y_pred_train))
 
-# Reestructuración de los datos para poder graficar
-y_df_fit = pd.DataFrame(y_fit, columns = ['Predicted value'])
-y_df_test = y_test.reset_index().drop('index', axis = 1)
-y_df_fit['Real value'] = y_df_test
-y_df_fit.reset_index(inplace=True)
+# Prediccion  usando el modelo sobre de los x_test 
+y_pred = model.predict(X_test)
 
-# Grafica sobre datos predichos y datos reales
-y_df_fit.plot(x="index", y=["Real value", "Predicted value"])
-plt.show()
+# Mean Square Error de los datos predichos y los reales
+print("Mean square error del modelo en datos test:", mean_squared_error(y_test, y_pred))
+
+# Accuracy del modelo
+print("Accuracy del modelo en datos test:", accuracy_score(y_test, y_pred))
+
+#Heat map de los datos obtenidos contra los reales
+cm_df = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm_df, annot=True, cmap='Blues')
+
+#Matriz de confusion de los datos
+cm_df = pd.DataFrame(confusion_matrix(y_test, y_pred), columns = ['Yes','No'])
+cm_df[''] = ['Yes', 'No']
+cols = cm_df.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+cm_df = cm_df[cols]
+cm_df = cm_df.set_index('')
+print('Confusion matrix: \n')
+print(cm_df)
+
